b5ebf3b9a641e0073e11614c0443311aeb3d41b8
kim-myeongju <kmj042894@gmail.com>
2025-09-05 17:58:46 +0900

    접근 제한 페이지 테스트 용 메서드 추가 - #2

--- BookAt-Service.zip_expanded/BookAt-Service/src/main/java/com/bookat/controller/UserLoginController.java
+++ BookAt-Service.zip_expanded/BookAt-Service/src/main/java/com/bookat/controller/UserLoginController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bookat.dto.FindPassword;
 import com.bookat.dto.UserLoginRequest;
@@ -31,7 +32,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
+//@Controller
+@RestController
 //@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserLoginController {
@@ -171,5 +173,10 @@ public class UserLoginController {
 	@PostMapping("/api/user/changePassword")
 	public String changePassword() {
 		return "user/resultPwForm";
+	}
+	
+	@PostMapping("/api/pay/test")
+	public String payTest() {
+		return "접근 제한 페이지 접근 테스트";
 	}
 }
